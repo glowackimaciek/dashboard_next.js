b05a332eb93084708996d09f4f8f9df6200a67a5
Maciej <macglowacki@gmail.com>
2024-04-06 16:22:48 +0200

    Dashboard Designe commit

--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,6 +1,97 @@
+import Image from "next/image"
+import styles from "@/app/ui/dashboard/transactions/transactions.module.css"
+
 const Transactions = () => {
     return (
-        <div>Transactions</div>
+        <div className={styles.container}>
+            <h2 className={styles.title}>Latest Transactions</h2>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <td>Name</td>
+                        <td>Status</td>
+                        <td>Date</td>
+                        <td>Amount</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image src="/image/noavatar.png" alt="" width={40} height={40} className={styles.userImage}/>
+                                Jhon Doe
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.done}`}>Done</span>
+                        </td>
+                        <td>
+                            14.02.2024
+                        </td>
+                        <td>
+                            $300
+                        </td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image src="/image/noavatar.png" alt="" width={40} height={40} className={styles.userImage}/>
+                                Jhon Doe
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.pending}`}>Pending</span>
+                        </td>
+                        <td>
+                            14.02.2024
+                        </td>
+                        <td>
+                            $300
+                        </td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image src="/image/noavatar.png" alt="" width={40} height={40} className={styles.userImage}/>
+                                Jhon Doe
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.cancelled}`}>Cancelled</span>
+                        </td>
+                        <td>
+                            14.02.2024
+                        </td>
+                        <td>
+                            $300
+                        </td>
+                    </tr>
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div className={styles.user}>
+                                <Image src="/image/noavatar.png" alt="" width={40} height={40} className={styles.userImage}/>
+                                Jhon Doe
+                            </div>
+                        </td>
+                        <td>
+                            <span className={`${styles.status} ${styles.pending}`}>Pending</span>
+                        </td>
+                        <td>
+                            14.02.2024
+                        </td>
+                        <td>
+                            $300
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 
